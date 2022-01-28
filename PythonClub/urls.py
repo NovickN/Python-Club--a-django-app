01b490b30183bc0a55506291288ba1e856f1c36f
NovickN <86983647+NovickN@users.noreply.github.com>
2022-01-27 19:14:10 -0800

    Update urls.py

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -18,5 +18,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('club/', include('club.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
 ]
