f316c8b8205a9fea83bdc18cff440ed21a4923d4
NovickN <86983647+NovickN@users.noreply.github.com>
2022-01-31 08:55:44 -0800

    Update views.py

--- club/views.py
+++ club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event, User
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def users(request):
+    user_list=User.objects.all()
+    return render(request, 'club/users.html', {'user_list':user_list})
