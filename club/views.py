f8b06e92035476475f16caabd7a00ae1b6c171d2
NovickN <86983647+NovickN@users.noreply.github.com>
2022-02-21 15:12:39 -0800

    Update views.py

--- club/views.py
+++ club/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event, User
 from django.urls import reverse_lazy
+from .forms import MeetingForm, ResourceForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -17,4 +19,35 @@ def meetings(request):
 def meetingdetail(request, id):
     meeting=get_object_or_404(Meeting, pk=id)
     return render(request, 'club/meetingdetail.html', {'meeting' : meeting})
- 
+
+@login_required 
+def newResource(request):
+     form=ResourceForm
+     if request.method=='POST':
+          form=ResourceForm(request.POST)
+          if form.is_valid():
+               post=form.save(commit=True)
+               post.save()
+               form=ResourceForm()
+     else:
+          form=ResourceForm()
+     return render(request, 'club/newresource.html', {'form': form})
+
+@login_required  
+def newMeeting(request):
+     form=MeetingForm
+     if request.method=='POST':
+          form=MeetingForm(request.POST)
+          if form.is_valid():
+               post=form.save(commit=True)
+               post.save()
+               form=MeetingForm()
+     else:
+          form=MeetingForm()
+     return render(request, 'club/newmeeting.html', {'form': form})
+
+def loginmessage(request):
+    return render(request, 'club/loginmessage.html')
+
+def logoutmessage(request):
+    return render(request, 'club/logoutmessage.html')
