b623f45462c5f1cf9d84cbbd795879a71b5fecaf
NovickN <86983647+NovickN@users.noreply.github.com>
2022-02-21 15:13:02 -0800

    Update urls.py

--- club/urls.py
+++ club/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
    path('resources/', views.resources, name='resources'),
    path('meetings/', views.meetings, name='meetings'),
    path('meetingdetail/<int:id>', views.meetingdetail, name='detail'),
+   path('newresource/', views.newResource, name='newresource'),
+   path('newmeeting/', views.newMeeting, name='newmeeting'),
+   path('loginmessage/', views.loginmessage, name='loginmessage'),
+   path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 ]
